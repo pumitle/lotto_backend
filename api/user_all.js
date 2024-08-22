eaa6f69c1ebb01f1caad1949d1646279d59b8794
pumitle <65011212026@msu.ac.th>
2024-08-22 23:47:53 +0700

    up

--- api/user_all.js
+++ api/user_all.js
@@ -149,7 +149,36 @@ router.put("/addmoney", (req, res) => {
     });
 });
 
+//แก้ไขข้อมูลสมาชิก
+router.put("/eiditprof" , async (req,res) => {
+    const { name, email, password, phone, image } = req.body;
 
+    if (!name || !email || !password || !phone || !image) {
+        return res.status(400).json({ message: "All fields are required." });
+    }
+
+    try{
+
+        const {uid} = req.body;
+
+        const updateQuery = `UPDATE User SET name = ? , email = ? ,password = ? ,phone = ? , image = ? WHERE Uid = ?`;
+
+        const result = await conn.query(updateQuery, [name,email,password,phone,image,uid]);
+
+        if (result.affectedRows > 0) {
+            res.status(404).json({ message: "User not found." });
+            
+        }else {
+          
+            res.status(200).json({ message: "User information updated successfully." });
+        }
+
+    } catch(e) {
+        console.error("Error updating user information:", e);
+        res.status(500).json({ message: "An error occurred while updating user information." });
+    }
+
+});
 
 // ส่งออก router
 module.exports = { router };
